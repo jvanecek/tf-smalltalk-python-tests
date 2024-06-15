593e5c9f52b82b9c5ae537eb28737fe2b4793b49
Juan Vanecek <juann.vanecek@gmail.com>
2024-06-15 00:39:30 -0300

    add Conv2D tests

--- Convolution2DTests.py
+++ Convolution2DTests.py
@@ -47,11 +47,59 @@ class Convolution2D(unittest.TestCase):
 
         self.assertEqual(x.shape, (1, 4, 4, 1))
 
-        filter = tf.constant([[
-            [[0.11, 0.12], [0.21, 0.22]],
-            [[1.11, 1.12], [1.21, 1.22]]]],
-            dtype=tf.float32)
-        self.assertEqual(filter.shape, (1, 2, 2, 2))
+        filter = filter = tf.constant([
+            [
+                [[0.11, 0.12]],
+                [[0.21, 0.22]]
+            ],
+            [
+                [[1.11, 1.12]],
+                [[1.21, 1.22]]
+            ]
+        ], dtype=tf.float32)
+        self.assertEqual(filter.shape, (2, 2, 1, 2))
+
+        model_cnn = Sequential([
+            Conv2D(
+                filters=2,
+                kernel_size=(2, 2),
+                input_shape=x.shape[1:],
+                kernel_initializer=tf.keras.initializers.Constant(value=filter),
+                use_bias=False)
+        ])
+
+        y = model_cnn(x)
+
+        self.assertEqual(y.shape, (1, 3, 3, 2))
+
+        np.testing.assert_allclose(y.numpy(), [[
+            [[53.660004, 54.32], [56.300003, 57.], [58.940002, 59.68]],
+            [[80.06, 81.12], [82.700005, 83.8], [85.34, 86.48]],
+            [[106.46001, 107.92], [109.100006, 110.600006], [111.740005, 113.28]]
+        ]])
+    
+    
+    def testConv2DAsLayerActivatedByReLU(self):
+        x = tf.constant([[
+            [[11], [12], [13], [14]],
+            [[21], [22], [23], [24]],
+            [[31], [32], [33], [34]],
+            [[41], [42], [43], [44]]
+        ]], dtype=tf.float32)
+
+        self.assertEqual(x.shape, (1, 4, 4, 1))
+
+        filter = filter = tf.constant([
+            [
+                [[0.11, 0.12]],
+                [[0.21, 0.22]]
+            ],
+            [
+                [[1.11, 1.12]],
+                [[1.21, 1.22]]
+            ]
+        ], dtype=tf.float32)
+        self.assertEqual(filter.shape, (2, 2, 1, 2))
 
         model_cnn = Sequential([
             Conv2D(
@@ -97,7 +145,7 @@ class Convolution2D(unittest.TestCase):
                 input_shape=x.shape[1:],
                 kernel_initializer=tf.keras.initializers.Constant(value=filter),
                 use_bias=False),
-            MaxPooling2D(pool_size=(2, 2))
+            MaxPooling2D()
         ])
 
         y = model_cnn(x)
